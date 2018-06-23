7812c9f263b5e0c168654a624f1adf9554f8dab1
Rostislav Kirillov <superrast@gmail.com>
2018-06-23 16:07:44 +0500

    initial api commit

---
+++ Cards/Cards/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cards.Models;
+using EdjCase.JsonRpc.Router;
+using log4net;
+
+namespace Cards.Controllers
+{
+    public class TestController : RpcController
+    {
+        private readonly TestContext context;
+        private readonly ILog log;
+
+        public TestController(TestContext context, ILog log)
+        {
+            this.context = context;
+            this.log = log;
+        }
+
+        public List<Blog> List()
+        {
+            log.Debug($"total count: {context.Blogs.Count()}");
+            return context.Blogs.ToList();
+        }
+
+        public Blog Get(int id)
+        {
+            return context.Blogs.First(x => x.BlogId == id);
+        }
+
+        //POST /Test
+        //Example request using param list: {"jsonrpc": "2.0", "id": 1, "method": "Add", "params": [1,2]}
+        //Example request using param map: {"jsonrpc": "2.0", "id": 1, "method": "Add", "params": {"a": 1, "b": 2}}
+        public void Add(int id, string url)
+        {
+            var blog = new Blog
+            {
+                BlogId = id,
+                Url = url
+            };
+            context.Blogs.Add(blog);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Cards/Cards/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+
+namespace Cards
+{
+    public static class Logger
+    {
+        public static void Setup()
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+            var patternLayout = new PatternLayout
+            {
+                ConversionPattern = "%date [%thread] %-5level %logger - %message%newline"
+            };
+            patternLayout.ActivateOptions();
+
+            /*
+            var roller = new RollingFileAppender
+            {
+                AppendToFile = false,
+                File = @"Logs\log.txt",
+                Layout = patternLayout,
+                MaxSizeRollBackups = 5,
+                MaximumFileSize = "1GB",
+                RollingStyle = RollingFileAppender.RollingMode.Size,
+                StaticLogFileName = true
+            };
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+            */
+
+            var console = new ConsoleAppender
+            {
+                Layout = patternLayout
+            };
+            console.ActivateOptions();
+            hierarchy.Root.AddAppender(console);
+
+            hierarchy.Root.Level = Level.All;
+            hierarchy.Configured = true;
+        }
+    }
+}
